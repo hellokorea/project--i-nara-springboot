c3ea5f8e15f62f1e591c835afb43dc773a61aa41
kim-youngchul <kmosh1212@naver.com>
2024-10-14 14:17:41 +0900

    first commit

--- src/main/java/com/eureka/mindbloom/MindbloomApplication.java
+++ src/main/java/com/eureka/mindbloom/MindbloomApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MindbloomApplication {
 
     public static void main(String[] args) {
+        System.out.println("mindbloom application");
         SpringApplication.run(MindbloomApplication.class, args);
     }
 
