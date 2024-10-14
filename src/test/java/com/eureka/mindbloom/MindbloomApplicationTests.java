ba5a78a7d74b649698ccc67990f949c91e668523
kim-youngchul <kmosh1212@naver.com>
2024-10-14 13:29:45 +0900

    first commit

---
+++ src/test/java/com/eureka/mindbloom/MindbloomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.mindbloom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindbloomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
