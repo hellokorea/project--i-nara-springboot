a7383e931e01ab6011b0fbed5340666da99d175d
kim-youngchul <kmosh1212@naver.com>
2024-10-21 00:39:14 +0900

    feat: 공통 코드 도메인 생성

---
+++ src/main/java/com/eureka/mindbloom/common/domain/code/CommonCode.java
@@ -0,0 +1,23 @@
+package com.eureka.mindbloom.common.domain.code;
+
+import com.eureka.mindbloom.common.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommonCode extends BaseEntity {
+
+    @EmbeddedId
+    private CommonCodeId id;
+
+    @MapsId("codeGroup")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "code_group")
+    private CommonCodeGroup codeGroup;
+
+    private String name;
+}
