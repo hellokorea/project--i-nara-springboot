c89bc5939eb7eabfcf8de087d2f88bf650b39fda
kim-youngchul <kmosh1212@naver.com>
2024-10-21 23:45:51 +0900

    refactor: ApiResponse datetime format 수정

--- src/main/java/com/eureka/mindbloom/common/dto/ApiResponse.java
+++ src/main/java/com/eureka/mindbloom/common/dto/ApiResponse.java
@@ -1,11 +1,16 @@
 package com.eureka.mindbloom.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public record ApiResponse<T>(String message, T data, LocalDateTime timestamp) {
+public record ApiResponse<T>(
+        String message,
+        T data,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+        LocalDateTime timestamp) {
 
     public static <T> ApiResponse<T> success(String message) {
         return new ApiResponse<>(message, null, LocalDateTime.now());
