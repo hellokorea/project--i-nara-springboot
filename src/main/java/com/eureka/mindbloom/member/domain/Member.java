9f5b98a8f6c17006fd06dba804b242c588416d79
13wjdgk <13wjdgk@naver.com>
2024-10-22 03:22:22 +0900

    feat : Child, Member Builder 추가

--- src/main/java/com/eureka/mindbloom/member/domain/Member.java
+++ src/main/java/com/eureka/mindbloom/member/domain/Member.java
@@ -1,33 +1,50 @@
 package com.eureka.mindbloom.member.domain;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String name;
+	private String name;
 
-    private String email;
+	private String email;
 
-    private String password;
+	private String password;
 
-    private String phone;
+	private String phone;
 
-    private String role;
+	private String role;
 
-    private LocalDateTime createdAt;
+	private LocalDateTime createdAt;
 
-    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Child> children;
+	@OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Child> children;
+
+	@Builder
+	public Member(String name, String email, String password, String phone, String role) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.role = role;
+		this.createdAt = LocalDateTime.now();
+	}
 }
