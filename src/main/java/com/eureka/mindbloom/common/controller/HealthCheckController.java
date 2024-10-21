375ba175259f49c14fcefc6bd673bf42b21364cb
kim-youngchul <kmosh1212@naver.com>
2024-10-21 23:32:03 +0900

    feat: health check api 작성

---
+++ src/main/java/com/eureka/mindbloom/common/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.eureka.mindbloom.common.controller;
+
+import com.eureka.mindbloom.common.dto.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok(ApiResponse.success("OK"));
+    }
+}
