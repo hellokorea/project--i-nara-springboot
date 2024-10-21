050d15206c106e99ae5f05dedae28a8a7adcbb14
kim-youngchul <kmosh1212@naver.com>
2024-10-21 16:47:11 +0900

    test: JwtProvider 클래스 테스트 작성

---
+++ src/test/java/com/eureka/mindbloom/auth/utils/JwtProviderTest.java
@@ -0,0 +1,93 @@
+package com.eureka.mindbloom.auth.utils;
+
+import com.eureka.mindbloom.auth.exception.AuthenticationException;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class JwtProviderTest {
+
+    private JwtProvider jwtProvider;
+
+    @BeforeEach
+    void setUp() {
+        String secretKey = "upluseurekaupluseurekaupluseurekaupluseurek";
+        jwtProvider = new JwtProvider(secretKey);
+    }
+
+    @Nested
+    class createToken {
+
+        @Test
+        void AccessToken_생성_성공() {
+            // given
+            String email = "test@gmail.com";
+            long expirationTime = 100000;
+
+            // when
+            String encodedToken = jwtProvider.createToken(email, expirationTime);
+
+            // then
+            assertThat(encodedToken).isNotNull();
+            assertThat(encodedToken.split("\\.")).hasSize(3);
+        }
+    }
+
+    @Nested
+    class parseToken {
+
+        @Test
+        void 토큰_검증_성공() {
+            // given
+            String email = "test@gmail.com";
+            long expirationTime = 100000;
+            String encodedToken = jwtProvider.createToken(email, expirationTime);
+
+            // when
+            Jws<Claims> claimsJws = jwtProvider.parseToken(encodedToken);
+
+            // then
+            assertThat(claimsJws.getPayload().getSubject()).isEqualTo(email);
+            assertThat(claimsJws.getHeader().getAlgorithm()).isEqualTo(Jwts.SIG.HS256.getId());
+        }
+
+        @Test
+        void 검증_실패_만료된_토큰(){
+            // given
+            String email = "test@gmail.com";
+            long expirationTime = 0;
+            String encodedToken = jwtProvider.createToken(email, expirationTime);
+
+            // when & then
+            assertThatThrownBy(() -> jwtProvider.parseToken(encodedToken))
+                    .isInstanceOf(ExpiredJwtException.class);
+        }
+
+        @Test
+        void 토큰_검증_실패_bearer_접두사_없는_토큰(){
+            // given
+            String fakeToken = "fake";
+
+            // when & then
+            assertThatThrownBy(() -> jwtProvider.parseToken(fakeToken))
+                    .isInstanceOf(AuthenticationException.class).hasMessage("토큰 'fake'는 유효하지 않습니다.");
+        }
+
+        @Test
+        void 토큰_검증_실패_유효하지_않은_토큰(){
+            // given
+            String faceTokenWithBearer = "Bearer fake";
+
+            // when & then
+            assertThatThrownBy(() -> jwtProvider.parseToken(faceTokenWithBearer))
+                    .isInstanceOf(AuthenticationException.class).hasMessage("토큰 'Bearer fake'는 유효하지 않습니다.");
+        }
+    }
+}
