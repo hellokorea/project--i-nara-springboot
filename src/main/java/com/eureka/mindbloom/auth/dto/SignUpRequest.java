96ecbcb1932c3856eafaa53cb58675ddd1282ef1
오유찬 <oyuchan50@gmail.com>
2024-10-22 10:16:25 +0900

    feat: 회원가입 기능 구현

---
+++ src/main/java/com/eureka/mindbloom/auth/dto/SignUpRequest.java
@@ -0,0 +1,31 @@
+package com.eureka.mindbloom.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SignUpRequest {
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "유요한 이메일 주소를 입력해주세요")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력갑입니다.")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
+            message = "비밀번호는 8~20자리수여야 합니다. 영문, 숫자, 특수문자를 1개 이상 포함해야 합니다.")
+    private String password;
+
+    @NotBlank(message = "이름은 필수 입력값입니다.")
+    @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하여야 합니다.")
+    private String name;
+
+    @Builder
+    public SignUpRequest(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/eureka/mindbloom/member/service/Impl/MemberServiceImpl.java
@@ -0,0 +1,44 @@
+package com.eureka.mindbloom.member.service.Impl;
+
+import com.eureka.mindbloom.auth.dto.SignUpResponse;
+import com.eureka.mindbloom.auth.dto.SignUpRequest;
+import com.eureka.mindbloom.auth.exception.DuplicationEmailException;
+import com.eureka.mindbloom.member.domain.Member;
+import com.eureka.mindbloom.member.repository.MemberRepository;
+import com.eureka.mindbloom.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public SignUpResponse signUp(SignUpRequest signUpRequestDto) {
+        if(memberRepository.existsByEmail(signUpRequestDto.getEmail())) {
+            throw new DuplicationEmailException("이미 존재하는 이메일 주소입니다.");
+        }
+
+        Member savedMember = createMemberFromDto(signUpRequestDto);
+        savedMember = memberRepository.save(savedMember);
+
+        return SignUpResponse.builder()
+                .memberId(savedMember.getId())
+                .name(savedMember.getName())
+                .email(savedMember.getEmail())
+                .build();
+    }
+
+    private Member createMemberFromDto(SignUpRequest dto) {
+        return Member.builder()
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(passwordEncoder.encode(dto.getPassword()))
+                .role("0400_02") // 기본 역할 설정
+                .build();
+    }
+}
