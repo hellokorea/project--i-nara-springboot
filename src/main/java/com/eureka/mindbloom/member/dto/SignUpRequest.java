13d6119f6779b127fdcdff37703f1eedadd8d4c1
오유찬 <oyuchan50@gmail.com>
2024-10-22 16:39:40 +0900

    chore: SignUpRequest 유효성 검사 메세지 개선

--- src/main/java/com/eureka/mindbloom/member/dto/SignUpRequest.java
+++ src/main/java/com/eureka/mindbloom/member/dto/SignUpRequest.java
@@ -1,25 +1,21 @@
 package com.eureka.mindbloom.member.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.validation.constraints.*;
+import lombok.*;
 
 @Getter
 public class SignUpRequest {
     @NotBlank(message = "이메일은 필수 입력값입니다.")
-    @Email(message = "유요한 이메일 주소를 입력해주세요")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
     private String email;
 
-    @NotBlank(message = "비밀번호는 필수 입력갑입니다.")
+    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,20}$",
-            message = "비밀번호는 8~20자리수여야 합니다. 영문, 숫자, 특수문자를 1개 이상 포함해야 합니다.")
+            message = "비밀번호는 8~20자리이며, 영문자, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다.")
     private String password;
 
     @NotBlank(message = "이름은 필수 입력값입니다.")
-    @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하여야 합니다.")
+    @Size(min = 2, max = 20, message = "이름은 2자 이상 20자 이하로 입력해주세요.")
     private String name;
 
     @Builder
