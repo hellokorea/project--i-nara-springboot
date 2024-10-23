c3ff127d86e7e1bb76968d1239a2f58d38354627
kim-youngchul <kmosh1212@naver.com>
2024-10-24 00:33:35 +0900

    feat: findByParentIdAndId 메서드 생성

--- src/main/java/com/eureka/mindbloom/member/repository/ChildRepository.java
+++ src/main/java/com/eureka/mindbloom/member/repository/ChildRepository.java
@@ -2,9 +2,19 @@ package com.eureka.mindbloom.member.repository;
 
 import com.eureka.mindbloom.member.domain.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
     List<Child> findByParentId(Long id);
+
+    @Query("""
+            select c from Child c
+            left join fetch c.preferredContents
+            where c.id = :id and c.parent.id = :parentId
+            """
+    )
+    Optional<Child> findByParentIdAndId(Long parentId, Long id);
 }
