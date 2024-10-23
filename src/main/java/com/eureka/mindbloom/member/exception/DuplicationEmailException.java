34553848d2eee213c26da830f9dd4eac564de181
오유찬 <oyuchan50@gmail.com>
2024-10-23 23:12:30 +0900

    refactor: 이메일 중복 예외 처리 로직 리팩토링

--- src/main/java/com/eureka/mindbloom/member/exception/DuplicationEmailException.java
+++ src/main/java/com/eureka/mindbloom/member/exception/DuplicationEmailException.java
@@ -1,10 +1,14 @@
 package com.eureka.mindbloom.member.exception;
 
-import com.eureka.mindbloom.common.exception.BaseException;
-import org.springframework.http.HttpStatus;
 
-public class DuplicationEmailException extends BaseException {
+public class DuplicationEmailException extends ConflictException {
+    private static final String DUPLICATION_EMAIL_MESSAGE = "이메일 '%s'는 이미 사용 중입니다.";
+
     public DuplicationEmailException(String message) {
-        super(message, HttpStatus.CONFLICT);
+        super(message);
+    }
+
+    public static DuplicationEmailException emailAlreadyExists(String email) {
+        return new DuplicationEmailException(String.format(DUPLICATION_EMAIL_MESSAGE, email));
     }
 }
