e5ed5efebbbfdd8869f54b25e71f037b0c9859fc
오유찬 <oyuchan50@gmail.com>
2024-10-23 23:13:13 +0900

    refactor: 유효성 검사 실패 로직 리팩토링

--- src/main/java/com/eureka/mindbloom/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/eureka/mindbloom/common/exception/GlobalExceptionHandler.java
@@ -6,8 +6,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,6 +22,23 @@ public class GlobalExceptionHandler {
         log.warn("source = {} {}, message = {}",
                 request.getMethod(), request.getRequestURI(), e.getMessage());
         return buildResponseEntity(e.getStatus(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationExceptions(
+            MethodArgumentNotValidException ex, HttpServletRequest request) {
+        log.warn("source = {} {}, message = {}",
+                request.getMethod(), request.getRequestURI(), ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ApiResponse.failure("유효성 검사 실패", errors));
     }
 
     @ExceptionHandler(Exception.class)
