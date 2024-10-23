e0eb927947734861e22b71b2e56f237740d85045
오유찬 <oyuchan50@gmail.com>
2024-10-24 01:46:01 +0900

    feat: 도서 좋아요 취소 기능 구현

--- src/main/java/com/eureka/mindbloom/book/service/BookLikeService.java
+++ src/main/java/com/eureka/mindbloom/book/service/BookLikeService.java
@@ -4,4 +4,5 @@ import com.eureka.mindbloom.book.domain.BookLike;
 
 public interface BookLikeService {
     BookLike addLike(String isbn, Long childId, String type);
+    void removeLike(String isbn, Long childId);
 }
