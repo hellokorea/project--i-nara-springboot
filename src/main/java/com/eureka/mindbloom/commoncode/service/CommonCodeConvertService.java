d560a612f58be9188b656a33cfec9bb2ea22077e
kim-youngchul <kmosh1212@naver.com>
2024-10-25 00:27:34 +0900

    feat: commonCodeConvertService 작성

---
+++ src/main/java/com/eureka/mindbloom/commoncode/service/CommonCodeConvertService.java
@@ -0,0 +1,47 @@
+package com.eureka.mindbloom.commoncode.service;
+
+import com.eureka.mindbloom.commoncode.domain.CommonCode;
+import com.eureka.mindbloom.commoncode.exception.CommonCodeNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommonCodeConvertService {
+
+    private final CommonCodeCacheService commonCodeCacheService;
+
+    public CommonCode codeNameToCommonCode(String codeName) {
+        List<CommonCode> caches = commonCodeCacheService.getAllCommonCodes();
+
+        return caches.stream()
+                .filter(cache -> codeName.equals(cache.getName()))
+                .findFirst()
+                .orElseThrow(()-> new CommonCodeNotFoundException(codeName));
+    }
+
+    public CommonCode codeToCommonCode(String code) {
+        List<CommonCode> caches = commonCodeCacheService.getAllCommonCodes();
+
+        return caches.stream()
+                .filter(cache -> code.equals(cache.getName()))
+                .findFirst()
+                .orElseThrow(()-> new CommonCodeNotFoundException(code));
+    }
+
+    public List<CommonCode> codeGroupNameToCommonCodes(String groupName) {
+        List<CommonCode> caches = commonCodeCacheService.getAllCommonCodes();
+
+        return caches.stream()
+                .filter(cache -> groupName.equals(cache.getCodeGroup().getGroupName())).toList();
+    }
+
+    public List<CommonCode> codeGroupToCommonCodes(String codeGroup) {
+        List<CommonCode> caches = commonCodeCacheService.getAllCommonCodes();
+
+        return caches.stream()
+                .filter(cache -> codeGroup.equals(cache.getCodeGroup().getCodeGroup())).toList();
+    }
+}
