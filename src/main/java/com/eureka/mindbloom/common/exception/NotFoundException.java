b2554b1b795a57399742f4154e1b266a30221f41
kim-youngchul <kmosh1212@naver.com>
2024-10-25 00:16:05 +0900

    chore: Exception, CommonCode 패키지 변경

--- src/main/java/com/eureka/mindbloom/common/exception/NotFoundException.java
+++ src/main/java/com/eureka/mindbloom/common/exception/NotFoundException.java
@@ -3,13 +3,7 @@ package com.eureka.mindbloom.common.exception;
 import org.springframework.http.HttpStatus;
 
 public class NotFoundException extends BaseException {
-    private static final String CHILD = "자녀 정보가 존재하지 않습니다. %s";
-
-    private NotFoundException(String message) {
+    public NotFoundException(String message) {
         super(message, HttpStatus.NOT_FOUND);
-    }
-
-    public static NotFoundException childNotFound(Long childId) {
-        return new NotFoundException(String.format(CHILD, childId));
     }
 }
