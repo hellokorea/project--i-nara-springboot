71b3414559335d6530eda5d5706064787e4a5592
kim-youngchul <kmosh1212@naver.com>
2024-10-25 02:56:09 +0900

    feat: 읽은 책 조회 api 작성 및 기본 url 변경 (member -> members)

--- src/main/java/com/eureka/mindbloom/book/controller/BookController.java
+++ src/main/java/com/eureka/mindbloom/book/controller/BookController.java
@@ -1,18 +1,19 @@
 package com.eureka.mindbloom.book.controller;
 
 import com.eureka.mindbloom.book.dto.BooksResponse;
+import com.eureka.mindbloom.book.dto.RecentlyBookResponse;
 import com.eureka.mindbloom.book.service.BookService;
 import com.eureka.mindbloom.book.service.SortOption;
 import com.eureka.mindbloom.common.dto.ApiResponse;
+import com.eureka.mindbloom.member.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/member/books")
+@RequestMapping("/members/books")
 @RequiredArgsConstructor
 public class BookController {
 
@@ -27,5 +28,16 @@ public class BookController {
     ) {
         Slice<BooksResponse> books = bookService.getBooks(categoryCode, search, page, sortOption);
         return ApiResponse.success("OK", books);
+    }
+
+    @GetMapping("/{childId}")
+    public ResponseEntity<?> getRecentlyBooks(
+            @AuthenticationPrincipal(expression = "member") Member member,
+            @PathVariable Long childId,
+            @RequestParam(defaultValue = "0") int page
+    ) {
+        RecentlyBookResponse response = bookService.getRecentlyViewedBooks(page, childId, member);
+
+        return ResponseEntity.ok().body(ApiResponse.success("OK", response));
     }
 }
