41ab08cc1a2ef9809ede7da916da21054392c6da
kim-youngchul <kmosh1212@naver.com>
2024-10-25 00:18:52 +0900

    refactor: Exception 생성 방식 변경

--- src/main/java/com/eureka/mindbloom/member/service/Impl/ChildServiceImpl.java
+++ src/main/java/com/eureka/mindbloom/member/service/Impl/ChildServiceImpl.java
@@ -1,13 +1,13 @@
 package com.eureka.mindbloom.member.service.Impl;
 
 import com.eureka.mindbloom.category.domain.ChildPreferred;
-import com.eureka.mindbloom.common.exception.NotFoundException;
 import com.eureka.mindbloom.member.domain.Child;
 import com.eureka.mindbloom.member.domain.Member;
 import com.eureka.mindbloom.member.dto.ChildProfileResponse;
 import com.eureka.mindbloom.member.dto.ChildRegisterRequest;
 import com.eureka.mindbloom.member.dto.ChildRegisterResponse;
 import com.eureka.mindbloom.member.dto.UpdateChildRequest;
+import com.eureka.mindbloom.member.exception.ChildNotFoundException;
 import com.eureka.mindbloom.member.repository.ChildRepository;
 import com.eureka.mindbloom.member.service.ChildService;
 import lombok.RequiredArgsConstructor;
@@ -56,7 +56,7 @@ public class ChildServiceImpl implements ChildService {
     @Override
     public void updateChildProfile(Member member, Long childId, UpdateChildRequest request) {
         Child child = childRepository.findByParentIdAndId(member.getId(), childId)
-                .orElseThrow(() -> NotFoundException.childNotFound(childId));
+                .orElseThrow(() -> new ChildNotFoundException(childId));
 
         List<ChildPreferred> preferred = request.categories().stream()
                 .map(category -> new ChildPreferred(category, child))
@@ -68,7 +68,7 @@ public class ChildServiceImpl implements ChildService {
     @Override
     public void deleteChildProfile(Member member, Long childId) {
         Child child = childRepository.findByParentIdAndId(member.getId(), childId)
-                .orElseThrow(() -> NotFoundException.childNotFound(childId));
+                .orElseThrow(() -> new ChildNotFoundException(childId));
 
         childRepository.delete(child);
     }
