ba47e0d0e8b8093d3a25625898cae6e03a9aad99
jiyun829 <rlacl829@pusan.ac.kr>
2024-10-29 13:07:08 +0900

    feat: admin cud기능 추가

--- src/main/java/com/eureka/mindbloom/book/controller/AdminBookController.java
+++ src/main/java/com/eureka/mindbloom/book/controller/AdminBookController.java
@@ -7,6 +7,7 @@ import com.eureka.mindbloom.book.util.BookFileProcessor;
 import com.eureka.mindbloom.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -22,6 +23,7 @@ public class AdminBookController {
 
     // 도서 등록
     @PostMapping("/")
+    @PreAuthorize("hasRole('ROLE_Admin')")
     public ResponseEntity<ApiResponse<AdminBookResponse>> registerBook(@RequestBody AdminBookRequest adminBookRequestDto) {
         AdminBookResponse savedBook = adminBookService.registerBook(adminBookRequestDto);
         return ResponseEntity.ok(ApiResponse.success("도서가 성공적으로 등록되었습니다.", savedBook));
@@ -29,6 +31,7 @@ public class AdminBookController {
 
     // 서버 경로에 있는 CSV 파일을 사용하여 도서 벌크 등록
     @PostMapping("/csv")//post로
+    @PreAuthorize("hasRole('ROLE_Admin')")
     public ResponseEntity<ApiResponse<String>> processCsvFile(@RequestParam String filePath) {
         try {
             bookFileProcessor.processCsvFileFromPath(filePath);
@@ -40,6 +43,7 @@ public class AdminBookController {
 
     // 도서 수정
     @PutMapping("/{isbn}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<ApiResponse<AdminBookResponse>> updateBook(@PathVariable String isbn, @RequestBody AdminBookRequest adminBookRequestDto) {
         Optional<AdminBookResponse> updatedBook = adminBookService.updateBook(isbn, adminBookRequestDto);
         return updatedBook.map(book -> ResponseEntity.ok(ApiResponse.success("도서가 성공적으로 수정되었습니다.", book)))
@@ -48,6 +52,7 @@ public class AdminBookController {
 
     // 도서 삭제
     @DeleteMapping("/{isbn}")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<ApiResponse<Void>> deleteBook(@PathVariable String isbn) {
         adminBookService.deleteBook(isbn);
         return ResponseEntity.ok(ApiResponse.success("도서가 성공적으로 삭제되었습니다."));
