d8704368108a580aab11702ab0ed1484dff89dd3
오유찬 <oyuchan50@gmail.com>
2024-11-03 16:47:23 +0900

    refactor: 상세 프로필 로직 수정

--- src/main/java/com/eureka/mindbloom/member/dto/ChildProfileResponse.java
+++ src/main/java/com/eureka/mindbloom/member/dto/ChildProfileResponse.java
@@ -2,12 +2,28 @@ package com.eureka.mindbloom.member.dto;
 
 import com.eureka.mindbloom.member.domain.Child;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public record ChildProfileResponse(
         Long childId,
         String childName,
-        String gender
+        String gender,
+        LocalDate birthDate,
+        int age, // 나이
+        List<String> categories
 ) {
     public static ChildProfileResponse from(Child child) {
-        return new ChildProfileResponse(child.getId(), child.getName(), child.getGender());
+
+        List<String> categories = child.getPreferredContents().stream()
+                .map(childPreferred -> childPreferred.getCategoryCode()) // category 필드 직접 사용
+                .collect(Collectors.toList());
+
+        // 나이 계산
+        int age = Period.between(child.getBirthDate(), LocalDate.now()).getYears();
+
+        return new ChildProfileResponse(child.getId(), child.getName(), child.getGender(), child.getBirthDate(), age, categories);
     }
 }
